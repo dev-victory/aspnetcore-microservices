009e145bf61f7ff56a9ccd36ff3032e6605d0d19
dev-victory <karansethi.official@gmail.com>
2023-05-30 16:23:52 +0530

    Added docker support for pgadmin and postgres db. Also modified program and startup files to configure required services

---
+++ src/Services/Discount/Discount.API/Program.cs
@@ -0,0 +1,19 @@
+namespace Discount.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            //host.MigrateDatabase<Program>();
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
