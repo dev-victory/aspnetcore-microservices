5ce05f853cafc95d98a99634ed772f8058011321
dev-victory <karansethi.official@gmail.com>
2023-05-31 19:03:13 +0530

    Added classes, folder and models for Application layer

---
+++ src/Services/Ordering/Ordering.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace Ordering.Application.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //CreateMap<Order, OrdersVm>().ReverseMap();
+            //CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+            //CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+        }
+    }
+}
