1de3ce02ecfa079c7bbc5ee755147ff54fef93ee
dev-victory <karansethi.official@gmail.com>
2023-06-15 15:46:28 +0530

    Added docker SQL container for OrderingAPI datastore. - Also, connected the API to the dockerized SQL instance. - Added EF migration classes - Added some seed data - Configured docker compose for the new SQL instance

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,25 +1,29 @@
-using Ordering.API;
+using Ordering.API.Extensions;
+using Ordering.Infrastructure.Persistence;
 
-public class Program
+namespace Ordering.API
 {
-    public static void Main(string[] args)
+    public class Program
     {
-        CreateHostBuilder(args)
-            .Build()
-            //.MigrateDatabase<OrderContext>((context, services) =>
-            //{
-            //    var logger = services.GetService<ILogger<OrderContextSeed>>();
-            //    OrderContextSeed
-            //        .SeedAsync(context, logger)
-            //        .Wait();
-            //})
-            .Run();
-    }
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args)
+                .Build()
+                .MigrateDatabase<OrderContext>((context, services) =>
+                {
+                    var logger = services.GetService<ILogger<OrderContextSeed>>();
+                    OrderContextSeed
+                        .SeedAsync(context, logger)
+                        .Wait();
+                })
+                .Run();
+        }
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
